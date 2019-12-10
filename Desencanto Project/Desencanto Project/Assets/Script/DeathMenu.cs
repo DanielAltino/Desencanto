a875df8d2bd2cf03c9d3ef056f1419def7b6ceb8
dparedes2 <dparedes@yepsolutions.com.br>
2019-12-10 11:28:31 -0300

    Game commit

---
+++ Desencanto Project/Desencanto Project/Assets/Script/DeathMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public string mainMenuLevel;
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitToMain()
+    {
+        Application.LoadLevel(mainMenuLevel);
+    }
+}
